0d7fc7136c40bfc90c56d9e411ca87a9908797e2
quocanhdk51 <quocanhdk51@gmail.com>
2021-03-30 14:21:15 +0700

    5 files modified, 18 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,18 @@ import { MatIconModule } from "@angular/material/icon";
 import { AppComponent } from "./app.component";
 import { SideNavComponent } from "./side-nav/side-nav.component";
 import { TopNavComponent } from "./top-nav/top-nav.component";
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 @NgModule({
-  declarations: [AppComponent, SideNavComponent, TopNavComponent],
+  declarations: [AppComponent, SideNavComponent, TopNavComponent, SignUpComponent, SignInComponent, HomeComponent, AboutComponent, ServicesComponent, ClientsComponent, ContactComponent, PageNotFoundComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
